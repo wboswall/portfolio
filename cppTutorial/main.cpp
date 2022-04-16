19128f69fb62394094270505b9d99672c1661f59
William Boswall <wboswall@live.com>
2022-04-16 14:48:22 -0300

    commit cppTutorial changes
    Decided to have on main.cpp and make the rest all header files.

---
+++ cppTutorial/main.cpp
@@ -0,0 +1,22 @@
+/*Name: CppTutorial
+ *File name: lessonOne.cpp
+ *Description: A series of C++ lessons for people who want to learn programming.
+ *Problem Specification:
+ *	-> Create a series of C++ lesson programs based of Sams Teach Yourself C++ in One Hour A Day
+ *  -> Contains header files for some lessons and will have the same lesson but will end in .h instead of .cpp
+ *Date Created: 04/16/22
+ *Date Released: 
+ *Date(s) Updated: 
+ *Version: 1.0.0.0
+ *Author: @wboswall
+ *License: MIT License 
+ *
+*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout  << "Hello World!" <<endl;
+	return 0;
+}
