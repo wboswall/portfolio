d59df3d6a9e7c376eca66a14be02615ba56bd865
William Boswall <wboswall@live.com>
2022-04-28 12:40:38 -0300

    Update cppLessons.h
    Added two simple void functions.

--- cppTutorial/cppLessons.h
+++ cppTutorial/cppLessons.h
@@ -1,1 +1,13 @@
+/*
+ *This is header file will have all of the lessons in classes.  
+*/
 
+void displayIntroduction()
+{
+    cout << "Welcome to the CPP Tutorial!/n" << endl;
+	  
+}
+void displayGoodLuckGreeting()
+{
+  cout  << "Hello World!" << endl;
+}
