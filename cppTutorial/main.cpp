58ac57e5f8f26f339bcdaa4a6ee93800e219fd4b
William Boswall <wboswall@live.com>
2022-04-28 12:28:39 -0300

    Update main.cpp
    Fixed some spacing and added in an output statement.

--- cppTutorial/main.cpp
+++ cppTutorial/main.cpp
@@ -15,8 +15,9 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-	cout  << "Hello World!" <<endl;
+	cout << "Welcome to the CPP Tutorial!/n" << endl;
+	cout  << "Hello World!" << endl;
 	return 0;
 }
